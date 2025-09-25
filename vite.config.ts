9a546c4b5250b59a50e42205e2309586193c064c
mohammadiqbalMaulana <iqbalmaulana14042005@gmail.com>
2025-09-25 10:47:59 +0700

    memulai memperbaiki struktur folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
 })
