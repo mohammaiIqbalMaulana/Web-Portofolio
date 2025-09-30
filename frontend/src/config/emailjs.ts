2f618122da94094cb5492b9b831381c4ba254d68
mohammadiqbalMaulana <iqbalmaulana14042005@gmail.com>
2025-09-30 15:18:16 +0700

    mengganti config

--- frontend/src/config/emailjs.ts
+++
@@ -1,6 +0,0 @@
-export const EMAILJS_CONFIG = {
-  SERVICE_ID: 'service_zrbnvqw',
-  CONTACT_TEMPLATE_ID: 'template_2shh9i3',
-  NEWSLETTER_TEMPLATE_ID: 'template_0vjgez5', 
-  PUBLIC_KEY: 'MyEN-v4XuExzqsvH6',
-};
---
+++ frontend/src/config/emailjs_example.ts
@@ -0,0 +1,6 @@
+export const EMAILJS_CONFIG = {
+  SERVICE_ID: 'your_service_id_here',
+  CONTACT_TEMPLATE_ID: 'your_contact_template_id_here',
+  NEWSLETTER_TEMPLATE_ID: 'your_newsletter_template_id_here', 
+  PUBLIC_KEY: 'your_public_key_here',
+};
